b5d338a32b25a1f4111a6c5780a647538a808636
유현 <yh7119@gmail.com>
2023-11-10 14:24:56 +0900

    Refactor: 요청 시 반환값 수정

--- trip_itinerary/src/main/java/com/example/trip_itinerary/itinerary/controller/ItineraryController.java
+++ trip_itinerary/src/main/java/com/example/trip_itinerary/itinerary/controller/ItineraryController.java
@@ -4,6 +4,8 @@ import com.example.trip_itinerary.itinerary.dto.request.save.ItinerarySaveReques
 import com.example.trip_itinerary.itinerary.dto.request.save.StaySaveRequest;
 import com.example.trip_itinerary.itinerary.dto.request.update.ItineraryPatchRequest;
 import com.example.trip_itinerary.itinerary.service.ItineraryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,13 +20,17 @@ public class ItineraryController {
     }
 
     @PostMapping("/{trip_id}/itinerary")
-    public Long saveItinerary(@PathVariable(name = "trip_id") Long id, @RequestBody @Validated ItinerarySaveRequest staySaveRequest) {
-        return itineraryService.saveItinerary(id, staySaveRequest);
+    public ResponseEntity<HttpStatus> saveItinerary(@PathVariable(name = "trip_id") Long id, @RequestBody @Validated ItinerarySaveRequest staySaveRequest) {
+        itineraryService.saveItinerary(id, staySaveRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PatchMapping("/itineraries/{itinerary_id}")
-    public Long patchItinerary(@PathVariable(name = "itinerary_id") Long id, @RequestBody @Validated ItineraryPatchRequest itineraryPatchRequest) {
-        return itineraryService.patchItinerary(id, itineraryPatchRequest);
+    @PatchMapping("/{trip_id}/itineraries/{itinerary_id}")
+    public ResponseEntity<HttpStatus> patchItinerary(@PathVariable(name = "itinerary_id") Long id, @RequestBody @Validated ItineraryPatchRequest itineraryPatchRequest) {
+        itineraryService.patchItinerary(id, itineraryPatchRequest);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
