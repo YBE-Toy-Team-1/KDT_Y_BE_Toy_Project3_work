b6fdcec31e25a266cc5a8eda57e4b941f4b43719
pabu <socks1583@gmail.com>
2023-11-16 14:44:41 +0900

    #51 검색 쿼리부분, 카카오서비스 수정

--- trip_itinerary/src/main/java/com/example/trip_itinerary/trip/controller/TripController.java
+++ trip_itinerary/src/main/java/com/example/trip_itinerary/trip/controller/TripController.java
@@ -1,7 +1,6 @@
 package com.example.trip_itinerary.trip.controller;
 
 
-import com.example.trip_itinerary.member.domain.Member;
 import com.example.trip_itinerary.member.domain.MemberAdapter;
 import com.example.trip_itinerary.trip.dto.request.TripUpdateRequest;
 import com.example.trip_itinerary.trip.dto.request.TripSaveRequest;
@@ -13,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
