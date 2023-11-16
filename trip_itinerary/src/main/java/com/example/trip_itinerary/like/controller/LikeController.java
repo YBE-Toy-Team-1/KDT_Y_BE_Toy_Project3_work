64ef2af559517bfd7e544da6de5fc531c34cea8c
유현 <yh7119@gmail.com>
2023-11-16 14:55:14 +0900

    Refactor: 스웨거 문서화를 위한 코멘트 커스텀

--- trip_itinerary/src/main/java/com/example/trip_itinerary/like/controller/LikeController.java
+++ trip_itinerary/src/main/java/com/example/trip_itinerary/like/controller/LikeController.java
@@ -4,6 +4,7 @@ import com.example.trip_itinerary.like.service.LikeService;
 import com.example.trip_itinerary.member.domain.Member;
 import com.example.trip_itinerary.member.domain.MemberAdapter;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,9 +19,12 @@ public class LikeController {
 
     private final LikeService likeService;
 
-    @Operation(summary = "여행 좋아요 등록/취소")
+    @Operation(summary = "여행 좋아요 등록/취소", description = "여행 정보에 좋아요를 표시하거나, 취소합니다.")
     @PostMapping("/trips/{trip_id}/like")
-    public ResponseEntity<HttpStatus> likeTrip(@PathVariable("trip_id") Long tripId, @AuthenticationPrincipal MemberAdapter memberAdapter) {
+    public ResponseEntity<HttpStatus> likeTrip(
+            @Parameter(description = "좋아요를 표시하거나 취소할 여행 ID", required = true, example = "1")
+            @PathVariable("trip_id") Long tripId,
+            @AuthenticationPrincipal MemberAdapter memberAdapter) {
         likeService.likeTrip(memberAdapter, tripId);
         return ResponseEntity.ok().build();
     }
